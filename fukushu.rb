49115b33c65fcbe92b4263ac3df460934acdaa4c
Todd Perry <todd.perry@myport.ac.uk>
2018-01-08 22:16:47 +0000

    added language select

--- fukushu.rb
+++ fukushu.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-def flash_card_question(set1, set2)
+def flash_card_question(set1, set2, language)
 	# pick random word from set 1
 	word_idxs = (0..set1.length-1).sort_by { rand }[0..5]
 	words = word_idxs.map { |x| set2[x] }
@@ -11,37 +11,57 @@ def flash_card_question(set1, set2)
 	user_answer = $stdin.gets.chomp
 	begin
 		if user_answer.to_i-1 == answer then
+			if language == "en" then
+				puts("Correct")
+			else
+				puts("正解！")
+			end
 			return true
 		end
 	rescue
 		# user entered something silly
 	end
+	if language == "en" then
+		puts("Incorrect")
+	else
+		puts("間違った！")
+	end
 	puts(words[answer])
 	return false
 end
 
-def text_entry_question(set1, set2)
+def text_entry_question(set1, set2, language)
 	word_idx = rand(0..set1.length-1)
 	puts(set1[word_idx] + " -->")
 	user_answer = $stdin.gets.chomp
 
 	if user_answer == set2[word_idx] then
+		if language == "en" then
+			puts("Correct")
+		else
+			puts("正解！")
+		end
 		return true
+	end
+	if language == "en" then
+		puts("Incorrect")
+	else
+		puts("間違った！")
 	end
 	puts(set2[word_idx])
 	return false
 end
 
-def do_test(set1, set2, reps, flash)
+def do_test(set1, set2, reps, flash, language)
 	correct = 0
 	(0..reps-1).each do |q_id|
 		if q_id > 0 then
 			puts("Current:" + correct.to_s + "/" + q_id.to_s)
 		end
 		if flash then
-			resp = flash_card_question(set1, set2)
+			resp = flash_card_question(set1, set2, language)
 		else
-			resp = text_entry_question(set1, set2)
+			resp = text_entry_question(set1, set2, language)
 		end
 		if resp then
 			correct = correct + 1
@@ -73,20 +93,50 @@ end
 
 def main_loop(vocab)
 	# menu
+	# set language
+	
+	language = ""
+	while language != "en" and language != "ja" do
+		puts("Choose Language")
+		puts("言語を選んでください")
+		puts("1: English/英語")
+		puts("2: Japanese/日本語")
+		language_id = $stdin.gets.chomp
+
+		if language_id == "1" then
+			language = "en"
+		elsif language_id == "2" then
+			language = "ja"
+		end
+	end
+
+
 	while true do
 		type = ""
 		while ((1..8).map { |x| x.to_s } + ["q"]).include?(type) == false do
-			puts("Pick Practise Type")
-			puts("1: English -> Japanese (Multi Choice)")
-			puts("2: English -> Japanese (Text Entry)")
-			puts("3: Japanese -> English (Multi Choice)")
-			puts("4: Japanese -> English (Text Entry)")
-			puts("5: Japanese -> Phonetic (Multi Choice)")
-			puts("6: Japanese -> Phonetic  (Text Entry)")
-			puts("7: Phonetic -> Japanese (Multi Choice)")
-			puts("8: Phonetic -> Japanese (Text Entry)")
-			puts("q: Quit")
-			puts("\n")
+			if language == "en" then
+				puts("Pick Practise Type")
+				puts("1: English -> Japanese (Multi Choice)")
+				puts("2: English -> Japanese (Text Entry)")
+				puts("3: Japanese -> English (Multi Choice)")
+				puts("4: Japanese -> English (Text Entry)")
+				puts("5: Japanese -> Phonetic (Multi Choice)")
+				puts("6: Japanese -> Phonetic  (Text Entry)")
+				puts("7: Phonetic -> Japanese (Multi Choice)")
+				puts("8: Phonetic -> Japanese (Text Entry)")
+				puts("q: Quit")
+			elsif language == "ja" then
+				puts("練習タイプを選んでください")
+				puts("1: 英語 -> 日本語 (六つ答えから選ぶ)")
+				puts("2: 英語 -> 日本語 (自由に答えを入力)")
+				puts("3: 日本語 -> 英語 (六つ答えから選ぶ)")
+				puts("4: 日本語 -> 英語 (自由に答えを入力)")
+				puts("5: 日本語 -> ひらがな (六つ答えから選ぶ)")
+				puts("6: 日本語 -> ひらがな  (自由に答えを入力)")
+				puts("7: ひらがな -> 日本語 (六つ答えから選ぶ)")
+				puts("8: ひらがな -> 日本語 (自由に答えを入力)")
+				puts("q: 閉じる")
+			end
 			type = $stdin.gets.chomp
 		end
 
@@ -97,7 +147,11 @@ def main_loop(vocab)
 
 		reps = 0
 		while reps == 0 do
-			puts("How Many Tests?")
+			if language == "en" then
+				puts("How Many Tests?")
+			elsif language == "ja" then
+				puts("問題数を入力してください")
+			end
 			reps = $stdin.gets.chomp.to_i
 		end
 
@@ -121,9 +175,9 @@ def main_loop(vocab)
 
 		if type.to_i % 2 == 0 then
 			# text entry test
-			do_test(set1, set2, reps, false)
+			do_test(set1, set2, reps, false, language)
 		else
-			do_test(set1, set2, reps, true)
+			do_test(set1, set2, reps, true, language)
 		end
 	end
 end
